6bbc97cbe607345420432feb7d6ddb7efc7ba8a0
vardhantandle1207 <106340287+vardhantandle1207@users.noreply.github.com>
2022-11-11 02:56:06 +0530

    Add files via upload

---
+++ charger/codes/1_1.py
@@ -0,0 +1,11 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+A = 12
+f = 50
+t = np.linspace(0, 2/f, 1000)
+plt.plot(t, np.abs(A*np.sin(2*np.pi*f*t)))
+plt.grid()
+plt.xlabel('t')
+plt.ylabel('x(t)')
+plt.show()
